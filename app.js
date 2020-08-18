6f7e572f8782b6909f5602e2aae2486b4376f462
M-Moein-M <mohamamdmoein.moradi@gamil.com>
2020-08-18 20:40:05 +0430

    Test commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+
+// add listener to port 3000
 app.listen(3000, ()=>console.log('listening to port'));
 
 app.use(express.static('public'));
