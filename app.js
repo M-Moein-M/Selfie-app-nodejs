ae98d7d35f9a145ed88cf52dcca597de8570283d
M-Moein-M <mohammadmoein.moradi@gmail.com>
2020-08-21 13:53:27 +0430

    Sending coordinates as post request back to the server

--- app.js
+++ app.js
@@ -2,22 +2,17 @@ const express = require('express');
 const app = express();
 
 // add listener for port 3000
-app.listen(3000, ()=>console.log('listening to port'));
+app.listen(3000, () => console.log('listening to port'));
 
 app.use(express.static('public'));
 
+// getting the post request
+app.use(express.json());
 
-const bodyParser = require('body-parser')
-
-app.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
-)
-
-app.use(bodyParser.json())
-
+/// handling post request
 app.post('/', (req, res) => {
-    console.log(req.body);
-})
+    let latitude = req.body.latitude;
+    let longitude = req.body.longitude;
+    console.log('lat: '+latitude.toString() + '\tlon: '+longitude.toString());
+});
 
