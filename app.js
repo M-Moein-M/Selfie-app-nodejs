dfaee91f2aa67e1d740a17ee23cc1a77b2383b5d
Moein <mohammadmoein.moradi@gmail.com>
2020-08-23 20:00:00 +0430

    Saving image contents in a file instead of database and then reading that file when sending back the data to client side(using nodejs file system)

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const fs = require('fs');
+
 const Datastore = require('nedb');
 
 // add listener for port 3000
@@ -9,19 +11,24 @@ app.listen(3000, () => console.log('listening to port'));
 app.use(express.static('public'));
 
 // getting the post request
-app.use(express.json({limit:'10mb'}));
+app.use(express.json({limit: '10mb'}));
 
 // saving the data received from the client using NeDB
 const fileName = 'database.db';
 let dataBase = new Datastore(fileName);
 dataBase.loadDatabase();
-console.log('Now storing data in '+fileName);
+console.log('Now storing data in ' + fileName);
 
 /// handling post request
 app.post('/api', (req, res) => {
     const data = req.body;
 
-    dataBase.insert(data);  // save new data to database
+    const imageFileName = createImageFileName(data); // creating unique code for each image file name
+    saveImageToFile(imageFileName, data.image64);
+
+    let editedData = data; // replace image data by image file name
+    editedData.image64 = imageFileName;
+    dataBase.insert(editedData);  // save new data to database
 
     // send something as respond
     res.json({
@@ -33,12 +40,30 @@ app.post('/api', (req, res) => {
 
 
 app.get('/api', function (req, res) {
-    dataBase.find({}, (err, data)=>{
-        if (err){ // just get out
+    dataBase.find({}, (err, data) => {  // get all the database
+        if (err) { // just get out
             res.end();
             return;
         }
-        res.json(data);
-    })
+        let outputData = []
+        for (let item of data) {
+            item.image64 = loadImageFromFile(createImageFileName(item));
+            outputData.push(item);
+        }
 
-})
+        res.json(outputData);
+    });
+
+});
+
+function createImageFileName(data) {
+    return 'image_' + data.timestamp.toString();
+}
+
+function loadImageFromFile(imageFileName) {
+    return fs.readFileSync(imageFileName, "utf-8");
+}
+
+function saveImageToFile(fileName, imageData) {
+    fs.writeFileSync(fileName, imageData);
+}
